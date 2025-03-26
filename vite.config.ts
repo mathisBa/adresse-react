462a3fa3b50885bad7fef6c54b1e07683d0d6dc3
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-03-26 12:05:55 +0100

    Turn it to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        orientation: "portrait",
+
+        start_url: "/",
+        name: "Vite App",
+        short_name: "Vite App",
+        description: "A Vite app with PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512.png",
+            type: "image/png"
+          },
+          {
+            purpose: "any",
+            sizes: "192x192",
+            src: "icon192.png",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
